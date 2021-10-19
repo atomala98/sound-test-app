4fa1c0235e43c14bd42eb2acfbd4656e807a44bb
andrzej <andrzej.tomala@comarch.com>
2021-10-19 15:55:13 +0200

    MSDPBP-8, admin acc model

--- soundtestapp/mainbackend/models.py
+++ soundtestapp/mainbackend/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
 
-# Create your models here.
 
 class ExaminedPerson(models.Model):
     first_name = models.CharField(max_length=30)
@@ -25,6 +24,7 @@ class Test(models.Model):
 
 
 class Exam(models.Model):
+    exam_name = models.CharField(max_length=30, null=True)
     test_id = models.ForeignKey(Test, on_delete=models.CASCADE)
 
 
@@ -37,3 +37,16 @@ class ExaminationResult(models.Model):
     def __str__(self):
         return str(self.person_id) + ": " + str(self.start_date)
 
+
+class AdminACC(models.Model):
+    login = models.CharField(max_length=20)
+    password = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+
+class AdminToExam(models.Model):
+    # Additional table for many to many Admin <-> Exam relation
+    admin_id = models.ForeignKey(AdminACC, on_delete=models.CASCADE)
+    exam_id = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    
