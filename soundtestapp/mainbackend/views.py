49461ce7ee535187b5f69eeed97edcef159615ed
atomala98 <andrzej.tomala1@gmail.com>
2021-10-29 17:57:55 +0200

    MSDPBP-3 - formularz rejestracyjny

--- soundtestapp/mainbackend/views.py
+++ soundtestapp/mainbackend/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
 from .models import ExaminationResult
 from django.template import loader
+from .forms import *
 
 def index(request):
-    first_person = ExaminationResult.objects.all()[0]
-    template = loader.get_template('mainbackend/index.html')
-    context = {
-        'first_person': first_person,
-    }
-    return HttpResponse(template.render(context, request))
+    form = RegisterForm()
+    return render(request, 'mainbackend/index.html', {'form': form})
