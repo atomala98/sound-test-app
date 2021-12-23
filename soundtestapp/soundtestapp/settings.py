4d3ad104b15e17a4c46e3bf595687436c156fa65
atomala98 <andrzej.tomala1@gmail.com>
2021-12-23 18:39:47 +0100

    sqlite3

--- soundtestapp/soundtestapp/settings.py
+++ soundtestapp/soundtestapp/settings.py
@@ -75,18 +75,18 @@ WSGI_APPLICATION = 'soundtestapp.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'tqrzzwcu',
-        'USER': 'tqrzzwcu',
-        'PASSWORD': 'EVgQA4jL501OwDr8Se_jsRODOrxP-fVm',
-        'HOST': 'surus.db.elephantsql.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'tqrzzwcu',
+    #     'USER': 'tqrzzwcu',
+    #     'PASSWORD': 'EVgQA4jL501OwDr8Se_jsRODOrxP-fVm',
+    #     'HOST': 'surus.db.elephantsql.com',
+    #     'PORT': '5432',
+    # }
 }
 
 # Password validation
