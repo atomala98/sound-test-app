0353346a113e5aff02d6d4ed4ad7fb496a493efa
atomala98 <andrzej.tomala1@gmail.com>
2021-12-23 17:24:00 +0100

    pre-deploy bugfixes

--- soundtestapp/soundtestapp/settings.py
+++ soundtestapp/soundtestapp/settings.py
@@ -75,12 +75,19 @@ WSGI_APPLICATION = 'soundtestapp.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tqrzzwcu',
+        'USER': 'tqrzzwcu',
+        'PASSWORD': 'EVgQA4jL501OwDr8Se_jsRODOrxP-fVm',
+        'HOST': 'surus.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
